661edc17af29166bf169d5835975fc8ac25171e8
Albert Huynh <albert.huynh@ibm.com>
2026-02-22 13:24:07 -0500

    fix frontend

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,8 +7,8 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 # number-flow for animated numbers
-pin "number-flow", to: "https://esm.sh/number-flow"
-pin "number-flow/group", to: "https://esm.sh/number-flow/group"
+pin "number-flow", to: "https://esm.sh/number-flow@0.5.10"
+pin "number-flow/group", to: "https://esm.sh/number-flow@0.5.10/group"
 
 # @floating-ui for tooltips
 pin "@floating-ui/core", to: "https://esm.sh/@floating-ui/core"
